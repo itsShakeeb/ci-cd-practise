8079edc8ba81e6ac44c6ba1e88e695205dcc6fb5
QL-Shakeeb-Arsalan <shakeeb.arsalan@quokkalabs.com>
2026-01-13 18:47:36 +0530

    added test file

---
+++ app/__test__/page.test.tsx
@@ -0,0 +1,12 @@
+import { describe, expect, it, } from 'vitest'
+import { render } from '@testing-library/react'
+import App from '../page'
+
+describe('Page Should Mount correctly', () => {
+    it('Page should run without exploding', () => {
+        const { getByText } = render(<App />)
+        const h1Element = getByText('To get started, edit the page.tsx file.')
+        expect(h1Element).toBeTruthy()
+
+    })
+})
